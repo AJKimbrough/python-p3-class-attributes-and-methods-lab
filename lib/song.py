c130c0ba68e51a120b7cc08ecff56a4a70955ebf
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-07-21 08:35:56 -0500

    complete lab

--- lib/song.py
+++ lib/song.py
@@ -4,7 +4,6 @@ class Song:
     count = 0
     genres = []
     artists = []
-    add_song_to_count = 0
     genre_count = {}
     genre = []
     artist_count = {}
@@ -13,47 +12,39 @@ class Song:
         self.name = name
         self.artist = artist
         self.genre = genre
-        Song.add_song_to_count += 1
-        Song.count +=1
-        Song.add_to_genres(self)
-        Song.add_to_artists(self)
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
         
     @classmethod
     def add_to_genres(cls, genre):
-        if genre in cls.genre_count:
-            #print(cls.genre_count)
-            cls.genre_count[genre.genre] += 1 
-        else:
-            cls.genres.append(genre.genre)
-            cls.genre_count[genre.genre] = 1
-        return (cls.genre_count)
+        if genre not in cls.genres:
+            cls.genres.append(genre)
 
     @classmethod
     def add_to_artists(cls, artist):
-        if artist in cls.genre:
-            print(cls.artist_count)
-            cls.artist_count[artist.artist] += 1 
-        else:
-            cls.artists.append(artist.artist)
-            cls.artist_count[artist.artist] = 1
-        return (cls.artist_count)
+        if artist not in cls.artists:
+            cls.artists.append(artist)
     
     @classmethod
-    def add_to_genre_count(cls):
-        return cls.genre_count
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+        return (cls.genre_count)
 
     @classmethod
-    def add_to_artist_count(cls):
-        pass
-
-
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+        return cls.artist_count
         
-
-song1 = Song("99 Problems", "Jay Z", "Rap")
-song2 = Song("Halo", "Beyonce", "Pop")
-song3 = Song("Smells Like Teen Spirit", "Nirvana", "Rock")
-song4 = Song("Encore", "Jay Z", "Rap")
-
-print(Song.genre_count)
-print(Song.artist_count)
-#print(Song.add_to_genre_count())
